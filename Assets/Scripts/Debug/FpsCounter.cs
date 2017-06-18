685f94813a1abe733d5dec5fcf34ccafe84b210d
alexander <st235@yandex.ru>
2017-06-18 12:20:37 +0300

    Fps calc fixes

--- Assets/Scripts/Debug/FpsCounter.cs
+++ Assets/Scripts/Debug/FpsCounter.cs
@@ -15,6 +15,10 @@ public class FpsCounter: MonoBehaviour
     void Update()
 	{
 		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        if (System.Math.Abs(deltaTime) < 0.001) {
+            Messenger<float>.Broadcast(EventsConfig.DebugFpsEvent, 60.0f);
+            return;
+        }
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
         Messenger<float>.Broadcast(EventsConfig.DebugFpsEvent, fps);
